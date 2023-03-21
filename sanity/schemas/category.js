74f524c3623a10b0198cbd2d3c38178ceae868b6
kaybrian <kayonbrian@gmail.com>
2023-03-21 18:46:25 +0200

    building the sanity backend

--- sanity/schemas/category.js
+++ sanity/schemas/category.js
@@ -2,18 +2,19 @@ import {defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'category',
-  title: 'Category',
+  title: 'Menu Category',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    }),
+    {
+      name:"name",
+      type:"string",
+      title:"Category Name",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name:"image",
+      type:"image",
+      title:"image of Category"
+    }
   ],
 })
---
+++ sanity/schemas/resturant.js
@@ -0,0 +1,67 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'resturant',
+  title: 'Resturant',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Resturant Name",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "short_description",
+      type: "string",
+      title: "short Description of the Resturant",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image of the Resturant",
+
+    },
+    {
+      name: "lat",
+      type: "number",
+      title: "latidude of the Resturant"
+    },
+    {
+      name: "long",
+      type: "number",
+      title: "longitude of the Resturant"
+    },
+    {
+      name: "address",
+      type: "string",
+      title: "address of the Resturant",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "rating",
+      type: "number",
+      title: "Enter a rate from (1-5 stars)",
+      validation: (Rule) =>
+        Rule.required()
+          .min(1)
+          .max(5)
+          .error("Please enter a number betweeb 1 and 5"),
+    },
+    {
+      name: "type",
+      title: "Category",
+      validation: (Rule) => Rule.required(),
+      type: "reference",
+      to: [{ type: "category" }],
+    },
+    {
+      name: "dishes",
+      type: "array",
+      title: "Dishes",
+      of: [{ type: "reference", to: [{ type: "dish" }] }],
+
+    }
+  ]
+})
